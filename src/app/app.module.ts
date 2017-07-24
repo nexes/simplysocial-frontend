3cbec08915ae5867e897df655ad5e705360fccbd
Joe Berria <joeberria@gmail.com>
2017-07-24 11:12:07 -0700

    cleanup from angular cli creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './components/authentication/user-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
