b2331d9e35804481100bf9651010d8d3a0506100
Joe Berria <joeberria@gmail.com>
2017-07-24 11:12:48 -0700

    created user login and user creation component

---
+++ src/app/components/authentication/user-login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-user-login',
+    templateUrl: 'user-login.component.html',
+    styleUrls: [ 'user-login.component.css' ]
+})
+export class UserLoginComponent implements OnInit {
+    private test: string;
+
+    constructor() {
+        this.test = 'hello ssdfasd';
+    }
+
+    ngOnInit() {
+
+    }
+}
