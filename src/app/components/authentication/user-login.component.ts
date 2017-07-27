87c8099329e587a774834f758789516c513c2f87
Joe Berria <joeberria@gmail.com>
2017-07-26 18:24:57 -0700

    update to handling user login

--- src/app/components/authentication/user-login.component.ts
+++ src/app/components/authentication/user-login.component.ts
@@ -1,32 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAuthService } from '../../services/user-auth.service';
 
 
 @Component({
     selector: 'app-user-login',
     templateUrl: 'user-login.component.html',
-    styleUrls: [ 'user-login.component.css' ]
+    styleUrls: [ 'user-login.component.css' ],
+    providers: [ UserAuthService ]
 })
 export class UserLoginComponent implements OnInit {
     private usernameOrEmail: string;
     private password: string;
     private rememberMe: boolean;
+    private errorTitle: string;
+    private errorMessage: string;
+    private showDialog: boolean;
 
-    constructor() {
+
+    constructor(private auth: UserAuthService) {
         this.rememberMe = false;
+        this.showDialog = false;
     }
 
     ngOnInit() {
-
     }
 
     login() {
-        console.log('login called');
-        console.log(this.usernameOrEmail);
-        console.log(this.password);
-        console.log(this.rememberMe);
+        this.auth.login(this.usernameOrEmail, this.password).subscribe(
+            (resp) => {
+                console.log('got data');
+                console.log(resp);
+            },
+            (err) => {
+                this.errorTitle = 'Login failed:';
+                this.errorMessage = err.error[ 'message' ];
+                this.showDialog = true;
+            },
+            () => {
+                // route from here
+                console.log('all done');
+            }
+        );
     }
 
-    newUser() {
-        console.log('new user called');
+    closeDialog() {
+        this.showDialog = false;
     }
 }
