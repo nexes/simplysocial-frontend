c1e8c75930ae39b68b9e3bd2a82aab38503a8ecf
Joe Berria <joeberria@gmail.com>
2017-07-30 22:48:40 -0700

    update to error dialog selector

--- src/app/components/dialog/error-dialog.component.ts
+++ src/app/components/dialog/error-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
-    selector: 'app-popup',
+    selector: 'app-error-dialog',
     templateUrl: 'error-dialog.component.html',
     styleUrls: ['error-dialog.component.css']
 })
