2a1ede9c252c77bb749ba1be91ac70e93c2a45ab
Joe Berria <joeberria@gmail.com>
2017-07-30 22:47:33 -0700

    created component for 404 page not found, init commit

---
+++ src/app/components/404/page-notfound.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-404',
+    templateUrl: 'page-notfound.component.html',
+    styleUrls: ['page-notfound.component.css']
+})
+export class PageNotFoundComponent {
+
+    constructor() {
+
+    }
+
+}
