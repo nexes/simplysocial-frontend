29943057dcf1de701805a2e05b2bc11e77a0c04f
Joe Berria <joeberria@gmail.com>
2017-07-30 22:49:33 -0700

    applying types to http responses. create user request

--- src/app/services/user-auth.service.ts
+++ src/app/services/user-auth.service.ts
@@ -3,10 +3,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 
-interface AuthResponse {
+export interface AuthResponse {
     code: number;
     message: string;
     userid: number;
+}
+
+export interface User {
+    firstname: string;
+    lastname: string;
+    username: string;
+    password: string;
+    email: string;
 }
 
 @Injectable()
@@ -19,11 +27,10 @@ export class UserAuthService {
     constructor(private http: HttpClient) {
         this.baseURL = 'http://localhost:8000/snaplife/api/auth/';
         this.headers = new HttpHeaders();
-
         const cookie = document.cookie;
 
         // only call out to our server if we don't have the csrf token set
-        if (cookie.length === 0) {
+        if (!cookie.includes('csrftoken')) {
             this.ensureCSRFToken();
         } else {
             if (cookie.indexOf(';') === -1) {
@@ -38,7 +45,7 @@ export class UserAuthService {
         }
     }
 
-    ensureCSRFToken() {
+    private ensureCSRFToken() {
         const req = this.http.get(this.baseURL + 'csrftoken/').subscribe(
             data => { },
             (err) => { /*this url will always return a status_code 200. This just ensures the browser has our csrf token*/ },
@@ -66,5 +73,17 @@ export class UserAuthService {
         };
 
         return this.http.post<AuthResponse>(url, data, { headers: this.headers });
+    }
+
+    logoff(username: string): Observable<AuthResponse> {
+        // TODO: logoff
+        return null;
+    }
+
+    newUser(user: User): Observable<AuthResponse> {
+        const url = this.baseURL + 'user/create/';
+        console.log(this.headers);
+
+        return this.http.post<AuthResponse>(url, user, { headers: this.headers });
     }
 }
