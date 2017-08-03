77e4910176224c526d7c6327ca756771b4b94b24
Joe Berria <joeberria@gmail.com>
2017-08-03 15:13:12 -0700

    handling error and validating for user creation

--- src/app/components/authentication/user-create.component.ts
+++ src/app/components/authentication/user-create.component.ts
@@ -1,14 +1,14 @@
 import { OnInit, Component } from '@angular/core';
+import { Router } from '@angular/router'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserAuthService, AuthResponse, User } from '../../services/user-auth.service';
-
+import { UserDataService } from '../../services/user-data.service';
 
 
 @Component({
     selector: 'app-create-user',
     templateUrl: 'user-create.component.html',
-    styleUrls: [ 'user-create.component.css' ],
-    providers: [ UserAuthService ]
+    styleUrls: [ 'user-create.component.css' ]
 })
 export class UserCreateComponent implements OnInit {
     private newUserForm: FormGroup;
@@ -16,11 +16,11 @@ export class UserCreateComponent implements OnInit {
     private errorTitle: string;
     private errorMsg: string;
 
-    constructor(private fb: FormBuilder, private auth: UserAuthService) {
+    constructor(private fb: FormBuilder, private userData: UserDataService, private auth: UserAuthService, private router: Router) {
         this.newUserForm = this.fb.group({
-            fullname: [ '', [ Validators.maxLength(80), Validators.required ] ],
+            fullname: [ '', [ Validators.minLength(2), Validators.maxLength(80), Validators.required ] ],
             email: [ '', [ Validators.email, Validators.required ] ],
-            username: [ '', [ Validators.maxLength(40), Validators.required ] ],
+            username: [ '', [ Validators.minLength(2), Validators.maxLength(40), Validators.required ] ],
             password: [ '', [ Validators.minLength(6), Validators.required ] ]
         });
     }
@@ -37,32 +37,52 @@ export class UserCreateComponent implements OnInit {
 
     createUser() {
         const name: string = this.newUserForm.get('fullname').value;
-        const [first, ...last] = name.split(' ');
+        const [firstName, ...last] = name.split(' ');
+        const lastName = last.join(' ');
 
-        if (first.length > 40 || last.join(' ').length > 40) {
+        if (firstName.length > 40 || lastName.length > 40) {
             this.errorTitle = 'Oops: Name Error';
-            this.errorMsg = 'The first and last name needs to be less than 40 characters';
+            this.errorMsg = 'The first and last name needs to be less than 40 characters each';
             this.showError = true;
+
+            this.newUserForm.get('password').reset();
+            return;
+        }
+        if (lastName.length < 1) {
+            this.errorTitle = 'Oops: Name Error';
+            this.errorMsg = 'I didn\'t see a last name given';
+            this.showError = true;
+
+            this.newUserForm.get('password').reset();
             return;
         }
 
         const newUser: User = {
-            firstname: first,
-            lastname: last.join(' '),
+            firstname: firstName,
+            lastname: lastName,
             username: this.newUserForm.get('username').value,
             password: this.newUserForm.get('password').value,
             email: this.newUserForm.get('email').value
         };
 
-        // TODO: handle routing
         this.auth.newUser(newUser).subscribe(
             (resp: AuthResponse) => {
-                console.log('all done');
+                this.router.navigate([ '/profile/', newUser.username ]).then(() => {
+                    this.userData.updateUser({
+                        username: newUser.username,
+                        firstname: newUser.firstname,
+                        lastname: newUser.lastname,
+                        email: newUser.email,
+                        userid: resp.userid
+                    });
+                });
                 console.log(resp);
             },
             (err) => {
-                console.log('error');
-                console.log(err);
+                this.errorTitle = 'Oops';
+                this.errorMsg = err.error[ 'message' ];
+                this.showError = true;
+                this.newUserForm.get('password').reset();
             }
         );
     }
