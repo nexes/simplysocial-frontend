a250d70a0ef0babff15da9cf689d7a5ad1669acd
Joe Berria <joeberria@gmail.com>
2017-08-04 08:52:23 -0700

    importing material modules

--- src/app/app-ui.module.ts
+++ src/app/app-ui.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    MdButtonModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdMenuModule,
+    MdGridListModule,
+    MdInputModule,
+    MdCheckboxModule,
+    MdSlideToggleModule,
+    MdDialogModule
+} from '@angular/material';
+
 import { UserLoginComponent } from './components/authentication/user-login.component';
 import { UserCreateComponent } from './components/authentication/user-create.component';
 import { TimelineComponent } from './components/user/user-timeline.component';
@@ -9,10 +22,41 @@ import { ErrorDialogComponent } from './components/dialog/error-dialog.component
 import { PageNotFoundComponent } from './components/404/page-notfound.component';
 
 
+
+// We will import all the angular material modules we use here.
+@NgModule({
+    imports: [
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdToolbarModule,
+        MdIconModule,
+        MdMenuModule,
+        MdGridListModule,
+        MdInputModule,
+        MdCheckboxModule,
+        MdSlideToggleModule,
+        MdDialogModule
+    ],
+    exports: [
+        MdButtonModule,
+        MdToolbarModule,
+        MdIconModule,
+        MdMenuModule,
+        MdGridListModule,
+        MdInputModule,
+        MdCheckboxModule,
+        MdSlideToggleModule,
+        MdDialogModule
+    ]
+})
+export class AppMaterialComponentModule { }
+
+// We will import all of our components here
 @NgModule({
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        AppMaterialComponentModule
     ],
     declarations: [
         UserLoginComponent,
