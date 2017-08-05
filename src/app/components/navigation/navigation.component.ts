4220602dbec9aada51cda1519e6750173c438aeb
Joe Berria <joeberria@gmail.com>
2017-08-04 22:47:28 -0700

    updating navigation component to use angular material

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -8,24 +8,7 @@ import { Router } from '@angular/router';
     styleUrls: [ 'navigation.component.css' ]
 })
 export class NavigationComponent {
-    private showCreateUser: boolean;
-    private showPasswordReset: boolean;
-    private showIcon: boolean;
-
     constructor(private router: Router) {
 
-    }
-
-    selected(selected: string) {
-        if (this.router.url === '/newuser') {
-            this.showCreateUser = true;
-            this.showPasswordReset = false;
-            this.showIcon = true;
-
-        } else if (this.router.url === '/resetpassword') {
-            this.showCreateUser = false;
-            this.showPasswordReset = true;
-            this.showIcon = true;
-        }
     }
 }
