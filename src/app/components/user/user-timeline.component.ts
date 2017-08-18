89e345601e46b39b862907dab56c57040b06213e
Joe Berria <joeberria@gmail.com>
2017-08-17 21:33:57 -0600

    display image preview when creating a new post

--- src/app/components/user/user-timeline.component.ts
+++ src/app/components/user/user-timeline.component.ts
@@ -15,12 +15,14 @@ export class TimelineComponent implements OnInit {
     private showLoadingBar: boolean;
     private currentUser: CurrentUser;
     private imageData: string;
+    private hideImgPreview: boolean;
 
     constructor(private userPost: UserPostService,
                 private userData: UserDataService,
                 private dialog: ModalDialogService,
                 private navBar: NavBarService) {
         this.showLoadingBar = true;
+        this.hideImgPreview = true;
         this.navBar.showUserNavBar();
         this.userData.listen().subscribe(this.userDataServiceSuccess, this.userDataServiceError);
     }
@@ -48,8 +50,17 @@ export class TimelineComponent implements OnInit {
         reader.addEventListener('load', () => {
             const imgString: string = reader.result;
             this.imageData = imgString.substring(imgString.indexOf('base64,') + 'base64,'.length);
+
+            const img = <HTMLImageElement>document.getElementById('img-preview');
+            img.src = imgString;
+            this.hideImgPreview = false;
         });
         reader.readAsDataURL(file);
+    }
+
+    removeImagePreview() {
+        this.imageData = undefined;
+        this.hideImgPreview = true;
     }
 
     userDataServiceSuccess(resp: CurrentUser) {
