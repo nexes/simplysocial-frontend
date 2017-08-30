84413bf7ddbc80497f5fa98a27e938cbe25052ed
Joe Berria <joeberria@gmail.com>
2017-08-30 12:38:04 -0700

    avatar update from settings component

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserDataService, CurrentUser } from '../../services/user-data.service';
 import { NavBarService } from '../../services/navbar.service';
+import { ProcessImage } from '../../util/imageprocess';
 
 
 @Component({
@@ -9,6 +10,7 @@ import { NavBarService } from '../../services/navbar.service';
     styleUrls: ['settings.component.css']
 })
 export class SettingsComponent {
+    private imageProcess: ProcessImage;
     private userDescription: string;
     private userEmail: string;
     private userAvatar: string;
@@ -18,15 +20,34 @@ export class SettingsComponent {
     // get user info that can be changed in settings. and first/last name
     constructor(private userData: UserDataService, private navBar: NavBarService) {
         console.log('settings cstr()');
+        this.imageProcess = new ProcessImage();
         this.navBar.showUserNavBar();
 
         this.userAvatar = this.userData.avatar || 'assets/usericon.png';
-        this.userDescription = this.userData.description;
-        this.userEmail = this.userData.email;
-        this.userFullName = this.userData.name;
+        this.userDescription = this.userData.description || '';
+        this.userEmail = this.userData.email || '';
+        this.userFullName = this.userData.name || '';
     }
 
-    updateProfilePic() {
-        console.log('update');
+    updateProfilePic(avatarImg: File) {
+        console.log(avatarImg);
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            this.imageProcess.resizeImage(reader.result, 84, 84).subscribe((result) => {
+                this.userAvatar = result;
+            });
+        });
+
+        reader.readAsDataURL(avatarImg);
+    }
+
+    sendChanges() {
+        console.log('sendChanges()');
+        this.userData.updateUser({
+            avatar: this.userAvatar,
+            email: this.userEmail,
+            description: this.userDescription
+        });
     }
 }
