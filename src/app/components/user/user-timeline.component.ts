7587bee34dbef781b43bf4c02687c4821e6af75a
Joe Berria <joeberria@gmail.com>
2017-09-11 16:06:26 -0700

    handle showing posts from following users, handling default user avatars

--- src/app/components/user/user-timeline.component.ts
+++ src/app/components/user/user-timeline.component.ts
@@ -5,7 +5,7 @@ import { UserDataService, CurrentUser } from '../../services/user-data.service';
 import { UserAuthenticationService } from '../../services/user-auth.service';
 import { NavBarService } from '../../services/navbar.service';
 import { UserPostService, Post } from '../../services/user-post.service';
-import { UserFollowService } from '../../services/user-follow.service';
+import { UserFollowService, FollowUser } from '../../services/user-follow.service';
 import { ModalDialogService } from '../../components/dialog/modal-dialog.component';
 import { ProcessImage } from '../../util/imageprocess';
 
@@ -14,7 +14,7 @@ import { ProcessImage } from '../../util/imageprocess';
 @Component({
     selector: 'app-timeline',
     templateUrl: 'user-timeline.component.html',
-    styleUrls: ['user-timeline.component.css']
+    styleUrls: [ 'user-timeline.component.css' ]
 })
 export class TimelineComponent implements OnInit {
     private postImageData: string;
@@ -23,7 +23,7 @@ export class TimelineComponent implements OnInit {
     private currentUserAvatar: string;
     private showLoadingBar: boolean;
     private hideImgPreview: boolean;
-    private followList: Post[];
+    private followList: FollowUser[];
     private postList: Post[];
     private images: ProcessImage;
 
@@ -44,16 +44,22 @@ export class TimelineComponent implements OnInit {
         // populate the users timeline with their posts. This is getting called once - should it be a service?
         this.userPost.getUserPosts().subscribe((post: Post) => {
             this.postList = post[ 'posts' ];
+
+            for (const _post of this.postList) {
+                if (_post.authoravatar === '') {
+                    _post.authoravatar = 'assets/usericon.png'; // move this to the server
+                }
+            }
             this.showLoadingBar = false;
         });
 
         // populate the users following users
-        this.userFollowers.followList().subscribe((followers) => {
+        this.userFollowers.followList().subscribe((followers: FollowUser) => {
             this.followList = followers[ 'following' ];
 
             for (const _follower of this.followList) {
-                if (_follower['avatar'] === '') {
-                    _follower['avatar'] = 'assets/usericon.png';
+                if (_follower[ 'avatar' ] === '') {
+                    _follower[ 'avatar' ] = 'assets/usericon.png';
                 }
             }
         });
@@ -68,7 +74,12 @@ export class TimelineComponent implements OnInit {
         this.dialog.showNewPostDialog(this.postMessage).subscribe(
             (resp) => {
                 if (resp) {
-                    this.postList.unshift(resp['post']);
+                    const newPost = resp[ 'post' ];
+
+                    if (newPost.authoravatar === '') {
+                        newPost.authoravatar = 'assets/usericon.png';
+                    }
+                    this.postList.unshift(resp[ 'post' ]);
                 }
             }
         );
@@ -77,7 +88,12 @@ export class TimelineComponent implements OnInit {
     submitNewPost() {
         this.userPost.createPost(this.postMessage, this.postImageData).subscribe(
             (resp) => {
-                this.postList.unshift(resp[ 'post' ]);
+                const newPost = resp[ 'post' ];
+
+                if (newPost.authoravatar === '') {
+                    newPost.authoravatar = 'assets/usericon.png';
+                }
+                this.postList.unshift(newPost);
             },
             (err) => {
                 console.log(err);
@@ -123,10 +139,19 @@ export class TimelineComponent implements OnInit {
     userSearch() {
         this.dialog.showUserSearchDialog().subscribe(
             (searchResp: string) => {
+                if (searchResp === this.userData.username) {
+                    // can't follow yourself
+                    return;
+                }
+
                 this.userFollowers.followUser(searchResp).subscribe(
                     (resp) => {
+                        this.followList.push({
+                            avatar: resp.followavatar || 'assets/usericon.png',
+                            username: resp.followname
+                        });
                         this.userData.updateUser({ following: resp.followercount });
-                        this.snackBar.open(`following ${searchResp}`, 'dismiss', { duration: 3000 });
+                        this.snackBar.open(`following ${resp.followname}`, 'dismiss', { duration: 3000 });
                     },
                     (err) => {
                         console.log(err);
@@ -134,5 +159,9 @@ export class TimelineComponent implements OnInit {
                 );
             }
         );
+    }
+
+    followingUserSelect(username: string) {
+        console.log('follower clicked');
     }
 }
